e8871c804592aa49d9164c98403bc2d5804e2a76
Charan-SV <charanv@devtools.in>
2023-09-26 16:28:34 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,53 @@
-print("hello")
+import requests
+import csv
+
+# Set your GitLab project ID and access token
+PROJECT_ID = 41299063
+ACCESS_TOKEN = "glpat-XsRzi9SD8yHJyMavhhh3"
+
+# Define the API endpoint URL
+BASE_URL = f"https://gitlab.com/api/v4/projects/{PROJECT_ID}"
+
+# Function to get issue IDs
+def get_issue_ids():
+    issues_url = f"{BASE_URL}/issues"
+    response = requests.get(issues_url, headers={"PRIVATE-TOKEN": ACCESS_TOKEN})
+    if response.status_code == 200:
+        issues_data = response.json()
+        return [issue["iid"] for issue in issues_data]
+    else:
+        print(f"Failed to retrieve issue IDs. Status code: {response.status_code}")
+        return []
+
+# Function to retrieve notes for a specific issue
+def get_notes_for_issue(issue_id):
+    notes_url = f"{BASE_URL}/issues/{issue_id}/notes"
+    response = requests.get(notes_url, headers={"PRIVATE-TOKEN": ACCESS_TOKEN})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve notes for issue {issue_id}. Status code: {response.status_code}")
+        return []
+
+# Get issue IDs
+issue_ids = get_issue_ids()
+
+# Create a CSV file and write the data
+with open("gitlab_data.csv", mode="w", newline="") as csv_file:
+    fieldnames = ["Issue ID", "Note ID", "Username", "Note Body", "Note Created At"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for issue_id in issue_ids:
+        notes = get_notes_for_issue(issue_id)
+        for note in notes:
+            writer.writerow({
+                "Issue ID": issue_id,
+                "Note ID": note["id"],
+                "Username": note["author"]["username"],
+                "Note Body": note["body"],
+                "Note Created At": note["created_at"]
+            })
+
+print("Data has been saved to gitlab_data.csv")
+
